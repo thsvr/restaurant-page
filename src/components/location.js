146589ac4f830d07ceb82324b6f749119022cab3
thsvr <vr.ths.zd@gmail.com>
2020-02-10 18:38:25 -0300

    Add location style.

--- src/components/location.js
+++ src/components/location.js
@@ -1,7 +1,11 @@
 const Location = (() => {
   const renderLocation = () => {
     const content = document.getElementById('tabContent');
-    content.innerHTML = '<h2>Location content and more html code.</h2>';
+    content.innerHTML =`<article class="main-location"> 
+    <div class='description-location card'>
+      <h1 class="contact-text">Fill it with some adress</h1>
+    </div>
+  </article>`;
   };
   return {
     renderLocation,
--- src/components/menu.js
+++ src/components/menu.js
@@ -3,10 +3,12 @@ const Menu = (() => {
   const renderMenuPage = () => {
     const content = document.getElementById('tabContent');
     content.innerHTML = `<p> Menu content and more html code </p>
-<figure><img src='../components/img2.jpg' alt= 'cake is not showing up'></img></figure>
-<figure><img src='../public/img/img1.jpg' alt= '' ></img></figure>
-
-`;
+<figure>
+ <img src='../public/img/img2.jpg' alt= 'cake is not showing up'></img>
+</figure>
+<figure>
+ <img src='../public/img/img1.jpg' alt= '' ></img>
+</figure>`;
   };
   return {
     renderMenuPage,
