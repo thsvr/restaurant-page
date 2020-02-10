17eb38d37de97e71d4fa54d5d139f8209671b3fc
thsvr <vr.ths.zd@gmail.com>
2020-02-10 17:48:50 -0300

    Add img to menu - not done yet.

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,13 @@
+import img2 from '../components/img2.jpg';
+
 const Menu = (() => {
   const renderMenuPage = () => {
     const content = document.getElementById('tabContent');
-  	content.innerHTML = `<p> Menu content and more html code </p>`;
+    content.innerHTML = `<p> Menu content and more html code </p>
+<figure><img src='../components/img2.jpg' alt= 'cake is not showing up'></img></figure>
+<figure><img src='../public/img/img1.jpg' alt= '' ></img></figure>
+
+`;
   };
   return {
     renderMenuPage,
