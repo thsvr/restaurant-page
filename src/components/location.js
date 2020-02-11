3cd4be3818ff1d094e01c1a4cacd3785574a5bec
thsvr <vr.ths.zd@gmail.com>
2020-02-11 13:10:56 -0300

    REQUIRED CHANGES- Delete non necessary script code for Bootstrap, add better descriptionand author's name to in json file, reinstall npm for as a devDep, remove lodash and bootstrap from npm, add location.

--- src/components/location.js
+++ src/components/location.js
@@ -3,7 +3,9 @@ const Location = (() => {
     const content = document.getElementById('tabContent');
     content.innerHTML = `<article class="main-location"> 
     <div class='description-location card'>
-      <h1 class="contact-text">Fill it with some adress</h1>
+      <h1 class="contact-text">Do you wanna try the best chocolate ever? </h1>
+      <h2>000 Newbury St, Boston</h2> 
+      <h2>MA 02116, United States</h2>
     </div>
   </article>`;
   };
