e584840e6a11b2960f56a767ea62f12f576db868
thsvr <vr.ths.zd@gmail.com>
2020-02-11 17:32:54 -0300

    Refactor homepage code using createElement and appendChild.

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,11 +1,28 @@
 const Homepage = (() => {
   const renderHomepage = () => {
     const content = document.getElementById('tabContent');
-    content.innerHTML = `<article class="main"> 
-      <div class='description-main'>
-        <h1>Fill it with some description</h1>
-      </div>
-    </article>`;
+    content.innerHTML = '';
+
+    const main = document.createElement('article');
+    main.classList.add('main');
+    
+    const descriptionMain = document.createElement('div');
+    descriptionMain.classList.add('description-main');
+    main.append(descriptionMain);
+
+    const descriptionText = document.createElement('h2');
+    descriptionText.append('Welcome to Chocolate Suisse :)');
+    descriptionMain.append(descriptionText);
+
+
+    content.appendChild(main);
+
+
+    // content.innerHTML = `<article class="main"> 
+    //   <div class='description-main'>
+    //     <h1>Fill it with some description</h1>
+    //   </div>
+    // </article>`;
   };
   return {
     renderHomepage,
